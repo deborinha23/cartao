c39f162f65edef0cc13426bce2d2f16f5852ff08
deborinha23 <142243057+deborinha23@users.noreply.github.com>
2024-10-22 11:03:04 -0300

    Add files via upload

---
+++ bkpCart-oAlura-main/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+//categoria,
+'Programação'
+//pergunta,
+'O quw é JavaScript'
+//resposta,
+'O Javascript é uma línguagem de programação'
+)
+
+criaCartao(
+    //categoria,
+    'Programação'
+    //pergunta,
+    'O quw é JavaScript'
+    //resposta,
+    'O Javascript é uma línguagem de programação'
+    )
+
+    criaCartao(
+        //categoria,
+        'Programação'
+        //pergunta,
+        'O quw é JavaScript'
+        //resposta,
+        'O Javascript é uma línguagem de programação'
+        )
